a9624b63a9604969b80230619635363b1c01fb6e
PhilNorfleet <norfleet.phillip@gmail.com>
2015-02-19 13:47:29 -0500

    complete crud assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'advertisements/index'
+
+  get 'advertisements/show'
+
+  resources :advertisements
+
   resources :posts
 
   get 'about' => 'welcome#about'
