7f3806d562ed7b67ba8de7e4ffae99a2209ebc65
PhilNorfleet <norfleet.phillip@gmail.com>
2015-03-05 02:53:02 -0500

    adds voting

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,14 +5,14 @@ describe Vote do
     before do
       @up_vote = Vote.new( value: 1 )
       @down_vote = Vote.new( value: -1 )
-      @invalid_vote = Vote.new ( value : 2 )
+      @invalid_vote = Vote.new( value: 2 )
     end
 
     describe "value validation" do
       it "only allows -1 or 1 as values" do
         expect( @invalid_vote.valid? ).to eq( false )
         expect( @up_vote.valid? ).to eq( true )
-        expect( @down_vote.valid? ).to eq( true ) 
+        expect( @down_vote.valid? ).to eq( true )
       end
     end
   end
