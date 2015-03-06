ea94f9766fb11c2b790c1c992875331d808c9760
PhilNorfleet <norfleet.phillip@gmail.com>
2015-03-05 23:21:05 -0500

    adds tests for vote controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
 
-  after_create :create_vote
 
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
@@ -32,10 +31,9 @@ class Post < ActiveRecord::Base
   default_scope { order('rank DESC') }
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)
