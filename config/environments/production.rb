266310420fe7ea7c36ed9f043540448f89c1886d
Phil Norfleet <phil@phil.home>
2015-08-21 23:02:57 -0400

    changes name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'norfleet-bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'norfshare.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
 end
